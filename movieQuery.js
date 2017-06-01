3211f5b8099f60107ede6bfbc630a326c56c1e4e
cse101 <1420569@iub.edu.bd>
2017-06-01 10:56:42 +0600

    assignment5

--- movieQuery.js
+++ movieQuery.js
@@ -4,30 +4,43 @@ var movieList = [
   { title: "Omen : The Final Conflict", year: "1981", length: 108, rating: "6.7", genre: "horror" },
   { title: "Gladiator", year: "2000", length: 155, rating: "8.5", genre: "historical" },
   { title: "The Patriot", year: "2000", length: 165, rating: "7.1", genre: "historical" },
-  { title: "The Matrix Reloaded”, year: “2003", length: 138, rating: "7.2", genre: "sci-fi" }
+  { title: "The Shawshank Redemption", year: "1994", length: 142, rating: "9.3", genre: "drama" },
+  { title: "The Matrix Reloaded", year: "2003", length: 138, rating: "7.2", genre: "sci-fi" }
 ];
 
 function getMovieTitle( index ){
-  //add code
+  console.log(movieList[index].title);
 }
-
-console.log(getMovieTitle(3));
+console.log("name of the movie:");
+console.log(getMovieTitle(5));
 
 function addMovie( movie ){
-  //add code
+  movieList.push(movie);
+    console.log(movieList);
 }
 
-var newMovie = {};
+var newMovie = {title: "Hackshaw Ridge", year: "2017", length: 165, rating: "8.5", genre: "historical"};
 console.log(addMovie(newMovie));
 
 function movieByRating(){
-  //add code
+  movieList.sort(function(a, b) {
+    return ((a.rating) - (b.rating));
+});
+console.log(" Movie list after sort: " );
+  console.log(movieList);
 }
-
 console.log(movieByRating());
 
 function findByTitle( title ){
-  //add code
+  for(var i=0;i<movieList.length;i++)
+  {
+    var tmp=movieList[i].title.search(/Gladiator/)
+    if(tmp>0)
+    {
+      console.log(movieList[i].title);
+    
+    }
+  }
 }
 
-console.log(findByTitle("matrix"));
+console.log(findByTitle("Hackshaw"));
